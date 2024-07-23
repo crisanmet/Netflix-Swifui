5fa8fa8ca7b73f1b25b92029d2c7652bc6c03d0f
Cristian Sancricca <cristiansancricca@gmail.com>
2024-07-23 19:11:35 -0300

    Initial Commit

---
+++ NetflixClone/NetflixCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixCloneApp.swift
+//  NetflixClone
+//
+//  Created by Cristian Sancricca on 23/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct NetflixCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
