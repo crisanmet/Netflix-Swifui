4c468f32bb185846bc42ed978819a8894e6080eb
Cristian Sancricca <cristiansancricca@gmail.com>
2024-08-07 16:14:37 -0300

    Add Netflix hero view

---
+++ NetflixClone/Components/FrameReader.swift
@@ -0,0 +1,8 @@
+//
+//  FrameReader.swift
+//  NetflixClone
+//
+//  Created by Cristian Sancricca on 07/08/2024.
+//
+
+import Foundation
---
+++ NetflixClone/Components/VisualEffectBlur.swift
@@ -0,0 +1,8 @@
+//
+//  VisualEffectBlur.swift
+//  NetflixClone
+//
+//  Created by Cristian Sancricca on 07/08/2024.
+//
+
+import Foundation
